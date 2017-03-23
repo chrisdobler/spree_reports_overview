3e5f1a0a49fd508968537d2a91e74a3a3e4c71c5
Christopher Dobler <chrisdobler@gmail.com>
2017-03-22 18:18:40 -0700

    Adds Report Module

--- lib/spree_reports_overview.rb
+++ lib/spree_reports_overview.rb
@@ -1,2 +1,11 @@
 require 'spree_core'
+require 'spree_reports'
 require 'spree_reports_overview/engine'
+require 'spree_reports_overview/reports/base'
+require 'spree_reports_overview/reports/orders_overview'
+
+SpreeReports.reports = [
+  :orders_by_period,
+  :sold_products,
+  :orders_overview,
+]
