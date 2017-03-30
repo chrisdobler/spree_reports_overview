7fe785f3e4f4dddd532c84e7eb870ab38c7b06dd
Christopher Dobler <chrisdobler@gmail.com>
2017-03-29 19:16:18 -0700

    fixes to reports controller decorator

--- app/controllers/spree/admin/reports_controller_decorator.rb
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -4,11 +4,13 @@ Spree::Admin::ReportsController.class_eval do
 
   def orders_overview
     @report = SpreeReportsOverview::Reports::OrdersOverview.new(params)
-    format.html
-    format.csv {
-      return head :unauthorized unless SpreeReports.csv_export
-      send_data @report.to_csv, filename: @report.csv_filename, type: "text/csv"
-    }
+    respond_to do |format|
+      format.html
+      format.csv {
+        return head :unauthorized unless SpreeReports.csv_export
+        send_data @report.to_csv, filename: @report.csv_filename, type: "text/csv"
+      }
+    end
   end  
   
 end
